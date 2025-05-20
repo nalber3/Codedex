93fc463eeaaa0ea8189f38c6fe8718620d8b1c7b
nalber3 <nalber.3@gmail.com>
2025-05-19 20:47:39 -0600

    finished first draft of checkpoint codedex project

--- terminal_game.py
+++ terminal_game.py
@@ -1,5 +1,6 @@
 import random
 
+#player chooses between three characters, each with its own weakness and strengths
 character = int(input("Welcome to Lember, an RPG game of epic adventures!\n"
                       "...\n"
                       "...\n"
@@ -21,28 +22,32 @@ hp = 0
 damage = 0
 experience = 0
 
+#display of dictionaries showing stats
 
 if character == 1:
     profile = {
             "attackspeed": 1,
-            "hp": 40,
+            "hp": 60,
             "damage": 10,
+            "experience": 0,
 }
 
     print("You choose Warrior!")
 elif character == 2:
     profile = {
             "attackspeed": 3,
-            "hp": 25,
+            "hp": 38,
             "damage": 5,
+            "experience": 0,
 }
 
     print("You choose Archer!")
-elif character ==3:
+elif character == 3:
     profile = {
             "attackspeed": 2,
-            "hp": 15,
+            "hp": 38,
             "damage": 20,
+            "experience": 0,
 }
 
     print("You choose Wizard!")
@@ -50,6 +55,7 @@ elif character ==3:
 
 print(profile)
 
+#some brief lore
 print("Now defeat enough enemies to gain 20 xp in order to beat the game!\n"
       "Snakes are easier to fight and give small rewards,\n"
       "demons are the toughest, but their loot is big,\n"
@@ -60,14 +66,14 @@ print("Now defeat enough enemies to gain 20 xp in order to beat the game!\n"
 
 
 
-#Generates enemies until maxhealth = 0 or experience = 10, 
-#gives the player the option to not engage in battle
+#Generates enemies until health = 0 or experience = 50, 
+#gives the player the option to not engage in battle if not confident
 
 
 
 
 
-while experience < 100 and hp > 0:
+while profile["experience"] < 50 and profile["hp"] > 0:
 
 
     enemy = random.randint(1, 3)
@@ -84,44 +90,52 @@ while experience < 100 and hp > 0:
         if battle == 1:
             
 
-            while damageinflicted < 20:    
+            while damageinflicted < 15:    
 
+                #variance of damage
                 damageratio = random.randint(1, 3)
 
 
              
+                #Damage caused by the enemy
+                snakeattack = random.randint(1,2)
 
-                snakeattack = random.randint(1,3)
+                profile["hp"] -= snakeattack
 
-                hp -= snakeattack
+                damageinflicted += (profile["attackspeed"] * profile["damage"])*(damageratio)
 
-                damageinflicted += (attackspeed * damage)*(damageratio)
-        
-                if damageinflicted >= 20:
-
+                #condition to kill the enemy
+                if damageinflicted >= 15:
+                    
+                    #random generation of loot after killing the enemy
                     snakeloot = random.randint(1, 2)
 
                     if snakeloot == 1:
                         print(f"You killed the snake, lost {snakeattack} hp and found an apple, 5 hp recovered!\n"
                                "5 xp gained!")
-                        hp += 5
-                        experience += 5
+                        profile["hp"] += 5
+                        profile["experience"] += 5
+                        #shows current stats, including xp and hp
                         print(profile)
 
                     elif snakeloot == 2:
                         print(f"You killed the snake and lost {snakeattack} hp!\n"
                                "5 xp gained!")
-                        experience += 5
+                        profile["experience"] += 5
                         print(profile)
+
+        #goes back to the previous selection
         if battle == 2:
             print("Looking for more enemies ... ")        
 
-
-    if experience < 100 and hp <= 0:
+    #condition to lose
+    if profile["experience"] < 50 and profile["hp"] <= 0:
              
                 print("You died! :'( \n"
                     "Try again")
-    elif experience >= 100 and hp > 0:
+                print(f"{snakeattack} Damage received")
+    #condition to win
+    elif profile["experience"] >= 50 and profile["hp"] > 0:
                 print("Congratulations, you beat the game!")
 
         
@@ -138,44 +152,49 @@ while experience < 100 and hp > 0:
         if battle == 1:
             
 
-            while damageinflicted < 40:            
+            while damageinflicted < 30:
 
+                #variance of damage
                 damageratio = random.randint(1, 3)             
 
-                orcattack = random.randint(3,5)
+                orcattack = random.randint(2,4)
 
-                hp -= orcattack
+                profile["hp"] -= orcattack
 
-                damageinflicted += (attackspeed * damage)*(damageratio)
+                damageinflicted += (profile["attackspeed"] * profile["damage"])*(damageratio)
         
-                if damageinflicted >= 40:
+                if damageinflicted >= 30:
 
                     orcloot = random.randint(1, 3)
 
                     if orcloot == 1:
                         print(f"You killed the orc, lost {orcattack} hp and found two apples, 10 hp recovered!\n"
                                "10 xp gained!")
-                        hp += 10
-                        experience += 10
+                        profile["hp"] += 10
+                        profile["experience"] += 10
+                        print(profile)
 
                     elif orcloot == 2:
                         print(f"You killed the orc, lost {orcattack} hp and found a strenght potion, 5 damage increased!\n"
                                "10 xp gained!")
-                        experience += 10
-                        damage += 5
+                        profile["experience"] += 10
+                        profile["damage"] += 5
+                        print(profile)
 
                     elif orcloot == 3:
                         print(f"You killed the orc and lost {orcattack} hp!\n"
                                "10 xp gained!")
+                        print(profile)
                     
         if battle == 2:
             print("Looking for more enemies ... ")
 
-    if experience < 100 and hp <= 0:
+    if profile["experience"] < 50 and profile["hp"] <= 0:
              
                 print("You died! :'( \n"
                     "Try again")
-    elif experience >= 100 and hp > 0:
+                print(f"{orcattack} Damage received")
+    elif profile["experience"] >= 50 and profile["hp"] > 0:
                 print("Congratulations, you beat the game!")
 
 
@@ -192,47 +211,51 @@ while experience < 100 and hp > 0:
         if battle == 1:
             
 
-            while damageinflicted < 20:
+            while damageinflicted < 40:
             
-
+                #variance of damage
                 damageratio = random.randint(1, 3)             
 
-                demonattack = random.randint(15,50)
+                demonattack = random.randint(10,30)
 
-                hp -= demonattack
+                profile["hp"] -= demonattack
 
-                damageinflicted += (attackspeed * damage)*(damageratio)
+                damageinflicted += (profile["attackspeed"] * profile["damage"])*(damageratio)
         
-                if damageinflicted >= 50:
+                if damageinflicted >= 40:
 
                     demonloot = random.randint(1, 3)
 
                     if demonloot == 1:
                         print(f"You killed the demon, lost {demonattack} hp and found a health potion, 20 hp recovered!\n"
                                "30 xp gained!")
-                        hp += 20
-                        experience += 30
+                        profile["hp"] += 20
+                        profile["experience"] += 30
+                        print(profile)
 
                     elif demonloot == 2:
                         print(f"You killed the demon, lost {demonattack} hp and found two strenght potions, 10 damage increased!\n"
                                "30 xp gained!")
-                        experience += 30
-                        damage += 10
+                        profile["experience"] += 30
+                        profile["damage"] += 10
+                        print(profile)
 
-                    elif orcloot == 3:
+                    elif demonloot == 3:
                         print(f"You killed the demon, lost {demonattack} hp and found two apples and one strenght potion, 10 hp recovered and 5 damage increased!\n"
                                "30 xp gained!")
-                        experience += 30
-                        hp += 10
-                        damage += 5
+                        profile["experience"] += 30
+                        profile["hp"] += 10
+                        profile["damage"] += 5
+                        print(profile)
         
 
         if battle == 2:
             print("Looking for more enemies ... ")
 
-    if experience < 100 and hp <= 0:
+    if profile["experience"] < 50 and profile["hp"] <= 0:
              
                 print("You died! :'( \n"
                     "Try again")
-    elif experience >= 100 and hp > 0:
+                print(f"{demonattack} Damage received")
+    elif profile["experience"] >= 50 and profile["hp"] > 0:
                 print("Congratulations, you beat the game!")
